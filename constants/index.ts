d75741a4f1c79d03ee74d21d1f0e1e86fb223ca1
Shrushti Mane <shrusti@fathomable.in>
2022-10-05 12:34:47 +0530

    change

---
+++ constants/index.ts
@@ -0,0 +1,4 @@
+export const JWTSecret = "mysoftwareupdates-prototype";
+export const BaseURL = "https://adminpanel-decorate.herokuapp.com/";
+export const EmailAddress = "shrustimanexyz@gmail.com";
+export const EmailPassword = "delnttatsdkecuaa";
